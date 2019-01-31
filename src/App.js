50eafe0e950e465943623e85a5ee0993c101815d
Blake <bwhite1195@yahoo.com>
2019-01-30 21:27:36 -0500

    looped listings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "normalize.css/normalize.css";
 import "./App.scss";
+import JSON from "./data.json";
 import Header from "./components/Header/Header";
 import Filter from "./components/Filter";
 import Listing from "./components/Listings";
@@ -8,7 +9,9 @@ import Listing from "./components/Listings";
 class App extends Component {
   constructor(props) {
     super();
-    this.state = {};
+    this.state = {
+      listings: JSON
+    };
   }
 
   render() {
@@ -17,7 +20,7 @@ class App extends Component {
         <Header />
         <div className="wrapper">
           <Filter />
-          <Listing />
+          <Listing listings={this.state.listings} />
         </div>
       </div>
     );
--- src/components/Listings.js
+++ src/components/Listings.js
@@ -1,37 +1,56 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Listings extends Component {
-  render() {
-    return (
-      <section id="listings">
-        <div className="listing-header">
-          <input type="text" placeholder="Search" />
-          <div className="icons">
-            <i class="fas fa-list" />
-            <i class="fas fa-th" />
-          </div>
+const Listings = props => {
+  return (
+    <section id="listings">
+      <div className="listing-header">
+        <input type="text" placeholder="Search" />
+        <div className="icons">
+          <i className="fas fa-list" />
+          <i className="fas fa-th" />
         </div>
-        <div className="list-items">
-          <div className="item">
-            <img src="/img/house.jpeg" className="image" />
-            <div className="about">
-              <div className="price">
-                <i class="fas fa-dollar-sign" />
-                <span>500,000</span>
-              </div>
-              <div className="location">
-                <i class="fas fa-map-marker-alt" />
-                <span>New York</span>
-              </div>
-              <div className="favorite">
-                <i class="far fa-heart" />
+      </div>
+      <div className="list-items">
+        {props.listings.map(item => {
+          return (
+            <div className="item">
+              <img src={item.image} className="image" />
+              <div className="about">
+                <div className="price">
+                  <i className="fas fa-dollar-sign" />
+                  <span>{item.price}</span>
+                </div>
+                <div className="location">
+                  <i className="fas fa-map-marker-alt" />
+                  <span>{item.city}</span>
+                </div>
+                <div className="favorite">
+                  <i className="far fa-heart" />
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-      </section>
-    );
-  }
-}
+          );
+        })}
+      </div>
+    </section>
+  );
+};
 
 export default Listings;
+
+// <div className="item">
+//           <img src="/img/house.jpeg" className="image" />
+//           <div className="about">
+//             <div className="price">
+//               <i className="fas fa-dollar-sign" />
+//               <span>500,000</span>
+//             </div>
+//             <div className="location">
+//               <i className="fas fa-map-marker-alt" />
+//               <span>New York</span>
+//             </div>
+//             <div className="favorite">
+//               <i className="far fa-heart" />
+//             </div>
+//           </div>
+//         </div>
