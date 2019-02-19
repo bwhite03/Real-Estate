6334d0c46b2a1036271ed23f747aa8c4476b64b7
bwhite03 <bwhite1195@yahoo.com>
2019-02-18 20:22:22 -0500

    finished footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,35 @@ import React from "react";
 const Footer = () => {
   return (
     <footer id="footer">
-      <h1>Footer</h1>
+      <h1 className="footerLogo">Logo</h1>
+
+      <div className="contact">
+        <h1>Contact</h1>
+        <a href="#">info@email.com</a>
+        <a href="#">333-333-3333</a>
+        <ul>
+          <li>
+            <a href="#">
+              <i class="fab fa-facebook-square" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="fab fa-twitter-square" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="fab fa-google-plus-square" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="fab fa-tumblr-square" />
+            </a>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 };
